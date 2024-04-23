3bdbd72217c936c65bc82e7bd045f4997d57c7f9
Diego Beltran <beltrd@gmail.com>
2024-04-23 08:43:50 -0500

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        gray: {
+          100: "#FBFBFB",
+          200: "#EAEAEA",
+          300: "#DFDFDF",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#333333",
+          900: "#191919",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
